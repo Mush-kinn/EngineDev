63ddad1c7ea65a8d09b2d47175a6f566683b2586
Yaniel_Rivera-PC <yariverabrenes@fullsail.edu>
2017-08-30 17:16:49 -0400

    END Graphics System Base (WIP2)

--- EngineDev_GDD379/EngineDev_GDD379/Mush_Graphics.cpp
+++ EngineDev_GDD379/EngineDev_GDD379/Mush_Graphics.cpp
@@ -27,7 +27,7 @@ void Mush_Graphics::CreateDefaultCube(ID3D11Buffer **_vertBuffer){
 	};
 
 
-	VERTEX_Pos_Col newCube[36];
+	VERTEX_PosCol newCube[36];
 	// ft 0-1-2  1-3-2,  [2]
 	newCube[0] = { verts[0], colors[2] };
 	newCube[1] = { verts[1], colors[2] };
@@ -88,12 +88,117 @@ void Mush_Graphics::CreateDefaultCube(ID3D11Buffer **_vertBuffer){
 	ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
 	desc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_VERTEX_BUFFER;
 	desc.Usage = D3D11_USAGE::D3D11_USAGE_IMMUTABLE;
-	desc.ByteWidth = sizeof(VERTEX_Pos_Col) * 36;
+	desc.ByteWidth = sizeof(VERTEX_PosCol) * 36;
 	
 	D3D11_SUBRESOURCE_DATA data;
 	ZeroMemory(&data, sizeof(D3D11_SUBRESOURCE_DATA));
 	data.pSysMem = newCube;
 
-	iDevice->CreateBuffer(&desc, &data, _vertBuffer);
+	m_iDevice->CreateBuffer(&desc, &data, _vertBuffer);
 
+}
+
+void Mush_Graphics::CreateDeviceSwapChain(ID3D11Device **_device, IDXGISwapChain **_chain, ID3D11DeviceContext **_cntx, HWND &_window){
+
+	DXGI_SWAP_CHAIN_DESC chainDesc;
+	ZeroMemory(&chainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	chainDesc.BufferCount = 1;
+	chainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	chainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	chainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	chainDesc.BufferDesc.Height = (UINT)BACKBUFFER_HEIGHT;
+	chainDesc.BufferDesc.Width = (UINT)BACKBUFFER_WIDTH;
+	chainDesc.OutputWindow = _window;
+	chainDesc.SampleDesc.Count = 1;
+	chainDesc.SampleDesc.Quality = 0;
+	chainDesc.Windowed = TRUE;
+
+	D3D_FEATURE_LEVEL  FeatureLevelsRequested = D3D_FEATURE_LEVEL_10_0;
+	UINT               numLevelsRequested = 1;
+	D3D_FEATURE_LEVEL  FeatureLevelsSupported;
+
+#if _DEBUG
+	D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_DEBUG,
+		&FeatureLevelsRequested, numLevelsRequested, D3D11_SDK_VERSION, &chainDesc, &m_swapChain,
+		&m_iDevice, &FeatureLevelsSupported, &m_iDeviceContext);
+#else
+	D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL,
+		&FeatureLevelsRequested, numLevelsRequested, D3D11_SDK_VERSION, &chainDesc, &swapChain,
+		&iDevice, &FeatureLevelsSupported, &iDeviceContext);
+#endif
+
+	ID3D11Resource *iResource;
+	ZeroMemory(&iResource, sizeof(ID3D11Resource));
+	m_swapChain->GetBuffer(0, __uuidof(iResource), reinterpret_cast<void**>(&iResource));
+	m_iDevice->CreateRenderTargetView(iResource, NULL, &m_iRenderTarget);
+	iResource->Release();
+
+}
+
+void Mush_Graphics::SetDepthStencilBuffer(ID3D11Texture2D **_buffer){
+
+	D3D11_TEXTURE2D_DESC Stencil_Resource_desc;
+	ZeroMemory(&Stencil_Resource_desc, sizeof(D3D11_TEXTURE2D_DESC));
+	Stencil_Resource_desc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_DEPTH_STENCIL;
+	Stencil_Resource_desc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
+	Stencil_Resource_desc.Format = DXGI_FORMAT::DXGI_FORMAT_D32_FLOAT;
+	Stencil_Resource_desc.Height = (UINT)BACKBUFFER_HEIGHT;
+	Stencil_Resource_desc.Width = (UINT)BACKBUFFER_WIDTH;
+	Stencil_Resource_desc.ArraySize = 1;
+	Stencil_Resource_desc.SampleDesc.Count = 1;
+
+	m_iDevice->CreateTexture2D(&Stencil_Resource_desc, NULL, _buffer);
+}
+
+void Mush_Graphics::SetDepthStencilState(ID3D11DepthStencilState **_state){
+
+	D3D11_DEPTH_STENCIL_DESC state_desc;
+	ZeroMemory(&state_desc, sizeof(D3D11_DEPTH_STENCIL_DESC));
+
+	// Depth test parameters
+	state_desc.DepthEnable = true;
+	state_desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	state_desc.DepthFunc = D3D11_COMPARISON_LESS;
+
+	// Stencil test parameters
+	state_desc.StencilEnable = true;
+	state_desc.StencilReadMask = 0xFF;
+	state_desc.StencilWriteMask = 0xFF;
+
+	// Stencil operations if pixel is front-facing
+	state_desc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	state_desc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	state_desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	state_desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	// Stencil operations if pixel is back-facing
+	state_desc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	state_desc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	state_desc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	state_desc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+	
+	m_iDevice->CreateDepthStencilState(&state_desc, _state);
+
+}
+
+void Mush_Graphics::SetDepthStencilView(ID3D11DepthStencilView **_view, ID3D11Texture2D *_resource){
+
+	D3D11_DEPTH_STENCIL_VIEW_DESC STENCIL_DESC;
+	ZeroMemory(&STENCIL_DESC, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	STENCIL_DESC.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	STENCIL_DESC.Format = DXGI_FORMAT::DXGI_FORMAT_D32_FLOAT;
+
+	m_iDevice->CreateDepthStencilView(_resource, &STENCIL_DESC, _view);
+
+}
+
+void Mush_Graphics::SetRasterizerState(ID3D11RasterizerState **_rasterstate){
+
+	D3D11_RASTERIZER_DESC raster_desc;
+	ZeroMemory(&raster_desc, sizeof(D3D11_RASTERIZER_DESC));
+	raster_desc.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
+	raster_desc.CullMode = D3D11_CULL_MODE::D3D11_CULL_BACK;
+	raster_desc.DepthClipEnable = true;
+
+	m_iDevice->CreateRasterizerState(&raster_desc, _rasterstate);
 }
--- EngineDev_GDD379/EngineDev_GDD379/Mush_Graphics.h
+++ EngineDev_GDD379/EngineDev_GDD379/Mush_Graphics.h
@@ -2,6 +2,8 @@
 #include "stdafx.h"
 #include "SharedDefines.h"
 
+#define BACKBUFFER_WIDTH	1280.0f
+#define BACKBUFFER_HEIGHT	720.0f
 
 class Mush_Graphics
 {
@@ -10,43 +12,42 @@ private:
 	XMFLOAT4X4 m_view;
 	XMFLOAT4X4 m_Projection;
 	XMFLOAT4X4 m_CubeWorld;
-
-	XMFLOAT4X4 Spinny;
+	XMFLOAT4X4 m_Spinny;
 
 	// Vectors
-	XMFLOAT3 newCamOffset;
-	XMFLOAT3 Tracker_Up;
-	XMFLOAT3 Tracker_Pos;
-	XMFLOAT3 Tracker_Tgt;
+	XMFLOAT3 m_newCamOffset;
+	XMFLOAT3 m_Tracker_Up;
+	XMFLOAT3 m_Tracker_Pos;
+	XMFLOAT3 m_Tracker_Tgt;
 
 	// Buffers
-	ID3D11Buffer *vb_Cube;
-	ID3D11Buffer *ib_Cube;
-	ID3D11Buffer *cBuff_perspective;
-	ID3D11Buffer *cBuff_lighting;
+	ID3D11Buffer *m_vb_Cube;
+	ID3D11Buffer *m_ib_Cube;
+	ID3D11Buffer *m_cBuff_perspective;
+	ID3D11Buffer *m_cBuff_lighting;
 
 	// Layouts
-	ID3D11InputLayout *iLay_perspective;
+	ID3D11InputLayout *m_Lay_perspective;
 
 	// Shaders
-	ID3D11VertexShader *VertSha_perspective;
-	ID3D11PixelShader *PixSha_perspective;
+	ID3D11VertexShader *m_VertSha_perspective;
+	ID3D11PixelShader *m_PixSha_perspective;
 
 	// textures
-	ID3D11Texture2D *tx_UVMap;
+	ID3D11Texture2D *m_tx_UVMap;
 
 	// Views
-	ID3D11RenderTargetView *iRenderTarget;
-	ID3D11ShaderResourceView *iShaderView;
-	ID3D11DepthStencilView *iDepthStencilView;
+	ID3D11RenderTargetView *m_iRenderTarget;
+	ID3D11ShaderResourceView *m_iShaderView;
+	ID3D11DepthStencilView *m_iDepthStencilView;
 
 	// Pending...
-	ID3D11SamplerState *SampleState;
+	ID3D11SamplerState *m_SampleState;
 
-	ID3D11Device *iDevice;
-	ID3D11DeviceContext *iDeviceContext;
-	D3D11_VIEWPORT viewPort;
-	IDXGISwapChain *swapChain;
+	ID3D11Device *m_iDevice;
+	ID3D11DeviceContext *m_iDeviceContext;
+	D3D11_VIEWPORT m_viewPort;
+	IDXGISwapChain *m_swapChain;
 
 public:
 	Mush_Graphics();
@@ -64,14 +65,20 @@ public:
 		ID3D11RasterizerState *rasterState;
 	}default_pipeline;
 
+	void CreateDeviceSwapChain(ID3D11Device **_device, IDXGISwapChain **_chain, ID3D11DeviceContext **_cntx, HWND &_window);
+
 	void InitViewport(D3D11_VIEWPORT &_viewport, FLOAT _w, FLOAT _h,
 		FLOAT TopLeftX = 0, FLOAT TopLeftY = 0,
 		FLOAT _minDepth = 0, FLOAT MaxDepth = 1);
 
 	void SetPipeline(pipeline_state_t *_pipe);
 	void CreateDefaultCube(ID3D11Buffer **_vertBuffer);
+	void SetDepthStencilBuffer(ID3D11Texture2D ** _buffer);
+	void SetDepthStencilState(ID3D11DepthStencilState **_state);
+	void SetDepthStencilView(ID3D11DepthStencilView **_view, ID3D11Texture2D *_resource);
+	void SetRasterizerState(ID3D11RasterizerState **_rasterstate);
 
-	struct VERTEX_Pos_Col{
+	struct VERTEX_PosCol{
 		XMFLOAT3 pos;
 		XMFLOAT4 color;
 	};
