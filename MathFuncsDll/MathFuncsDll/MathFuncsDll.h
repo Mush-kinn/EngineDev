d0281c85a6b3c9dacd203959f55933f99f27807e
Yaniel_Rivera-PC <yariverabrenes@fullsail.edu>
2017-09-06 19:38:48 -0400

    Dynamic Library Walkthrough

---
+++ MathFuncsDll/MathFuncsDll/MathFuncsDll.h
@@ -0,0 +1,29 @@
+// MathFuncsDll.h
+// https://msdn.microsoft.com/en-us/library/ms235636(v=vs.120).aspx
+
+#ifdef MATHFUNCSDLL_EXPORTS
+#define MATHFUNCSDLL_API __declspec(dllexport) 
+#else
+#define MATHFUNCSDLL_API __declspec(dllimport) 
+#endif
+
+namespace MathFuncs
+{
+	// This class is exported from the MathFuncsDll.dll
+	class MyMathFuncs
+	{
+	public:
+		// Returns a + b
+		static MATHFUNCSDLL_API double Add(double a, double b);
+
+		// Returns a - b
+		static MATHFUNCSDLL_API double Subtract(double a, double b);
+
+		// Returns a * b
+		static MATHFUNCSDLL_API double Multiply(double a, double b);
+
+		// Returns a / b
+		// Throws const std::invalid_argument& if b is 0
+		static MATHFUNCSDLL_API double Divide(double a, double b);
+	};
+}
